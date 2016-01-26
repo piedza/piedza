3a9aa1639daf404b232f961ec6557ff3e7f191f2
Marleen Overkamp <marleen.overkamp@movements.nl>
2016-01-26 18:46:08 +0100

    Mijzelf toegevoegd (en een ignore voor VIM backups)

--- app.ts
+++ app.ts
@@ -13,7 +13,7 @@ class HelloWorld {
     names: string[];
     
     constructor() {
-        this.names = ['Chris', 'Petra', 'Ernst', 'Hans', 'Klaasjan', 'Arjen']
+        this.names = ['Chris', 'Petra', 'Ernst', 'Hans', 'Klaasjan', 'Arjen', 'Marleen']
     }
 }
 
